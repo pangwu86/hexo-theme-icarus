976e2cf3a1755699b29432f8a8a81816fc2090e9
pangwu86 <pangwu86@gmail.com>
2021-04-12 15:16:25 +0800

    update:阅读更多与标签长显示

--- layout/common/article.jsx
+++ layout/common/article.jsx
@@ -168,31 +168,32 @@ module.exports = class extends Component {
               />
             ) : null}
             <hr style="height:1px;margin:1rem 0" />
-            <div className="level is-mobile is-flex"></div>
-            {/* Tags */}
-            {page.tags && page.tags.length ? (
-              <div class="article-tags is-size-7 is-uppercase">
-                <i class="fas fa-tags has-text-grey"></i>&nbsp;
-                {page.tags.map((tag, index) => {
-                  return (
-                    <a class="link-muted" rel="tag" href={url_for(tag.path)}>
-                      {tag.name}
-                      {index !== page.tags.length - 1 ? ", " : ""}
-                    </a>
-                  );
-                })}
-              </div>
-            ) : null}
-            {/* "Read more" button */}
-            {index && page.excerpt ? (
-              <a
-                class="article-more button is-small is-size-7"
-                href={`${url_for(page.link || page.path)}#more`}
-              >
-                <i class="fas fa-book-reader has-text-grey"></i>&nbsp;&nbsp;
-                {__("article.more")}
-              </a>
-            ) : null}
+            <div className="level is-mobile is-flex">
+              {/* Tags */}
+              {page.tags && page.tags.length ? (
+                <div class="article-tags is-size-7 is-uppercase">
+                  <i class="fas fa-tags has-text-grey"></i>&nbsp;
+                  {page.tags.map((tag, index) => {
+                    return (
+                      <a class="link-muted" rel="tag" href={url_for(tag.path)}>
+                        {tag.name}
+                        {index !== page.tags.length - 1 ? ", " : ""}
+                      </a>
+                    );
+                  })}
+                </div>
+              ) : null}
+              {/* "Read more" button */}
+              {index && page.excerpt ? (
+                <a
+                  class="article-more button is-small is-size-7"
+                  href={`${url_for(page.link || page.path)}#more`}
+                >
+                  <i class="fas fa-book-reader has-text-grey"></i>&nbsp;&nbsp;
+                  {__("article.more")}
+                </a>
+              ) : null}
+            </div>
             {/* Share button */}
             {!index ? (
               <Share config={config} page={page} helper={helper} />
